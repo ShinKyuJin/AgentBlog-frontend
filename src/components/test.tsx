bc3b29a8267456e721392eab182de26455acf9a6
miminishin <miminishin@naver.com>
2020-05-10 17:20:49 +0900

    불필요한 파일 삭제

---
+++ src/components/test.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Test extends React.Component {
+  render() {
+    return (
+      <div>
+        Are you agent?
+      </div>
+    );
+  }
+}
+
+export default Test;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Test from './components/test';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Test />
   </React.StrictMode>,
   document.getElementById('root')
 );
