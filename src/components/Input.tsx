d8bf0abc458f94561bb9fc876b3490fd14a6b0f5
miminishin <miminishin@naver.com>
2020-05-18 18:17:19 +0900

    hook과 함께 사용하는 Input 컴포넌트

---
+++ src/components/Input.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.input`
+  border: none;
+  border: ${props => props.theme.boxBorder};
+  border-radius: ${props => props.theme.borderRadius};
+  background-color: ${props => props.theme.bgColor};
+  height: 35px;
+  font-size: 12px;
+  padding: 0px 15px;
+`
+
+type InputProps = {
+  placeholder?: string;
+  required?: boolean;
+  value: string;
+  onChange: any;
+  type?: string;
+  className?: string;
+}
+
+const Input = ({
+  placeholder,
+  required = true,
+  value,
+  onChange,
+  type,
+  className
+}: InputProps) => (
+  <Container
+    placeholder={placeholder}
+    required={required}
+    value={value}
+    onChange={onChange}
+    type={type}
+    className={className}
+  />
+)
+
+export default Input;
