41854200e8918b3c15197952060965186e2bdacf
miminishin <miminishin@naver.com>
2020-05-18 18:19:55 +0900

    input hook

---
+++ src/hooks/useInput.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react"
+
+export default (text: string) => {
+  const [value, setValue] = useState('');
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  }
+
+  return { value, onChange };
+}
