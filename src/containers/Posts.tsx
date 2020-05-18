11f68539d425ef32f05aed9120f9bb0626f5d770
miminishin <miminishin@naver.com>
2020-05-19 02:18:55 +0900

    파일 구조 변경중

---
+++ src/containers/Posts.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import styled from 'styled-components';
+import Post from './Post';
+
+const StyledPost = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`
+
+const Posts = () => {
+  return (
+    <StyledPost>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </StyledPost>
+  )
+}
+
+export default Posts;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
+import Posts from '../containers/Posts';
+import { Helmet } from 'react-helmet';
 
 const Container = styled.div`
+  @media (min-width: 1300px) {
+    width: 1300px;
+  }
+  @media (min-width: 1024px) {
+    width: 1024px;
+  }
   @media (min-width: 768px) {
     width: 768px;
   }
@@ -14,7 +22,10 @@ const Container = styled.div`
 const Home = () => {
   return (
     <Container>
-      Hello?
+      <Helmet>
+        <title>Agents Blog!</title>
+      </Helmet>
+      <Posts />
     </Container>
   )
 }
