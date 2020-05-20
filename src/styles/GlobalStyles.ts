0e8db5447598c6e3aea8d8d60c9fedb384e89a50
miminishin <miminishin@naver.com>
2020-05-20 18:24:30 +0900

    warning remove

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -1,8 +1,6 @@
 import { createGlobalStyle } from "styled-components";
-import reset from "styled-reset";
 
 const GlobalStyles = createGlobalStyle`
-  ${reset}
 
   * {
     box-sizing: border-box;
