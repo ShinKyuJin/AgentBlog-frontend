1a6569baf00248eb6482352ff267518eccfe28fe
miminishin <miminishin@naver.com>
2020-05-23 17:18:15 +0900

    Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,11 +2,9 @@ import React from "react";
 import styled from "styled-components";
 import { Helmet } from "react-helmet";
 
+
+
 const Container = styled.div`
-  width: 1024px;
-  margin: 0 auto;
-  background-color: #ffffff;
-  border: 1px solid black;
 `;
 
 const Home = () => {
