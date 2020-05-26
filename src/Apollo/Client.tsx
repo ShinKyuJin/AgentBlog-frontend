c56aa1986b7a31065f86682fb0d04b56fd54da0c
전현성 <skyoun97@gmail.com>
2020-05-26 16:55:35 +0900

    Done - Apollo Setting & LoginState

---
+++ src/Apollo/Client.tsx
@@ -0,0 +1,13 @@
+import ApolloClient from "apollo-boost";
+import { defaults, resolvers } from "./LocalState";
+
+export default new ApolloClient({
+  uri: "http://localhost:4000",
+  clientState: {
+    defaults,
+    resolvers,
+  },
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem("token")}`,
+  },
+});
