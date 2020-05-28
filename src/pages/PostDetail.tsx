cb17c9b58eba68aea456a9a7241536948e01a42c
miminishin <miminishin@naver.com>
2020-05-28 23:05:25 +0900

    codeblock

--- src/pages/PostDetail.tsx
+++ src/pages/PostDetail.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactMarkdown from "react-markdown";
 import styled from "styled-components";
 import { useParams } from "react-router-dom";
+import { gql } from "apollo-boost";
 
 interface PostDetailParams {
   username: string;
@@ -11,42 +12,14 @@ interface PostDetailParams {
 const Container = styled.div``;
 
 const markdown_input = `
+\`\`\`
+#include <stdio.h>
 
-# Live demo
-
-Changes are automatically rendered as you type.
-
-## Table of Contents
-
-* Implements [GitHub Flavored Markdown](https://github.github.com/gfm/)
-* Renders actual, "native" React DOM elements
-* Allows you to escape or skip HTML (try toggling the checkboxes above)]
-
-## HTML block below
-
-<blockquote>
-  This blockquote will change based on the HTML settings above.
-</blockquote>
-
-## How about some code?
-
-Pretty neat, eh?
-
-## Tables?
-
-| Feature   | Support |
-| --------- | ------- |
-| tables    | ✔ |
-| alignment | ✔ |
-| wewt      | ✔ |
-
-## More info?
-
-Read usage information and more on [GitHub](//github.com/rexxars/react-markdown)
-
----------------
-
-A component by [Espen Hovlandsdal](https://espen.codes/)
+int main(void) {
+  printf("Hello World\\n");
+  return 0;
+}
+\`\`\`
 `;
 
 const PostDetail = () => {
@@ -54,10 +27,46 @@ const PostDetail = () => {
 
   return (
     <Container>
-      <ReactMarkdown source={markdown_input} />
+      <ReactMarkdown 
+        source={markdown_input}
+        renderers={{
+          code: CodeBlock,
+          InlineCodeBlock: InlineCodeBlock,
+          blockquote: BlockQuoteBlock
+        }}
+        skipHtml={false}
+        escapeHtml={false}
+      />
       PostDetail username:{username}, posturl:{posturl}
     </Container>
   );
 };
+
+
+const InlineCodeBlock = (props: any) => {
+  return (
+    <span style={{background: '#ff0'}}>
+      {props.value}    
+    </span>
+  );
+}
+
+const BlockQuoteBlock = (props: any) => {
+  return (
+    <div style={{border: '1px dashed #aaa', borderRadius: 10, paddingLeft: 10, margin: 5}}>
+      {props.children}
+    </div>
+  );
+}
+
+const CodeBlock = (props: any) => {
+  return (
+    <pre style={{background: '#000', color: '#FFF', padding: 10}}>
+      <code>
+        {props.value}
+      </code>
+    </pre>
+  );
+}
 
 export default PostDetail;
