b83c490c0338877e442d312ae46ffdbf9ee31fc1
전현성 <skyoun97@gmail.com>
2020-05-31 21:51:02 +0900

    Doing - Setting Page

--- src/pages/Setting.tsx
+++ src/pages/Setting.tsx
@@ -4,16 +4,9 @@ import useInput from "../hooks/useInput";
 import Input from "../components/Input";
 
 const Container = styled.div``;
-const SearchInput = styled(Input)``;
 
 const Setting = () => {
-  const search = useInput("");
-  return (
-    <Container>
-      <SearchInput {...search} />
-      Setting
-    </Container>
-  );
+  return <Container>Setting</Container>;
 };
 
 export default Setting;
