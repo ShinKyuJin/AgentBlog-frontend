0a86f81fb02237995be6f9ce6974ec9fce6406b9
전현성 <skyoun97@gmail.com>
2020-06-07 04:59:38 +0900

    Doing - Uploading files

--- src/components/Uploader.tsx
+++ src/components/Uploader.tsx
@@ -1,11 +1,15 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import { useDropzone } from "react-dropzone";
 import styled from "styled-components";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { serverUri } from "../Apollo/Client";
 
 const Uploader = () => {
-  const onDrop = useCallback((acceptedFiles) => {
+  const [urlList, setUrlList] = useState<string[]>([]);
+  const onDrop = useCallback(async (acceptedFiles) => {
     console.log(acceptedFiles);
-    acceptedFiles.forEach((file: any) => {
+    const test = acceptedFiles.map(async (file: any) => {
       const reader = new FileReader();
 
       reader.onabort = () => console.log("aborted");
@@ -15,10 +19,35 @@ const Uploader = () => {
         console.log(binaryStr);
       };
       reader.readAsArrayBuffer(file);
+      const url = await uploadPhoto(file);
+      setUrlList([...urlList, url]);
+      return url;
     });
+    console.log(test);
   }, []);
 
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+  const uploadPhoto = async (file: any) => {
+    const formData = new FormData();
+    formData.append("file", file, file.originalname);
+
+    try {
+      const { data } = await axios.post(serverUri + "/api/upload", formData, {
+        headers: {
+          "content-type": "multipart/form-data",
+        },
+      });
+      console.log(data.location);
+      return data.location;
+    } catch (err) {
+      toast.error("파일 업로드에 실패하였습니다." + err);
+      return null;
+    }
+  };
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    accept: "image/*",
+    onDrop,
+  });
 
   return (
     <Container {...getRootProps()}>
@@ -26,8 +55,12 @@ const Uploader = () => {
       {isDragActive ? (
         <p>Drop the files here ...</p>
       ) : (
-        <FileUploader>Upload your file here!</FileUploader>
+        <FileUploader>Upload your file here! </FileUploader>
       )}
+      {urlList &&
+        urlList.map((url) => (
+          <img src={url} key={url} width={32} height={32} />
+        ))}
     </Container>
   );
 };
@@ -40,6 +73,6 @@ const FileUploader = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-`
+`;
 
 export default Uploader;
