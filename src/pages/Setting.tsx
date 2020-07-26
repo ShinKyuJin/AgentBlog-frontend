60f7394c57243b721b258313e91358267e238679
전현성 <skyoun97@gmail.com>
2020-07-27 00:00:01 +0900

    [Doing] - setting

--- src/pages/Setting.tsx
+++ src/pages/Setting.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import styled from "styled-components";
 
-const Container = styled.div``;
-
 const Setting = () => {
   return <Container>Setting</Container>;
 };
+
+const Container = styled.div`
+  ${(prop) => prop.theme.responsiveContainer}
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  margin-top: 2rem;
+`;
 
 export default Setting;
