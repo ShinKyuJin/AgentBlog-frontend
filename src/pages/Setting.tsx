5b935257e4113d92a3d2b7015b29c300374d6227
전현성 <skyoun97@gmail.com>
2020-07-27 00:33:22 +0900

    [Doing] - setting

--- src/pages/Setting.tsx
+++ src/pages/Setting.tsx
@@ -1,8 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import Button from "../components/Button";
+import FatText from "../components/FatText";
+import DetailText from "../components/DetailText";
+import Input from "../components/Input";
 
 const Setting = () => {
-  return <Container>Setting</Container>;
+  const [isProfileEditing, SetIsProfileEditing] = useState<boolean>(false);
+
+  return (
+    <Container>
+      <Profile>
+        <ImageEditContainer>
+          <Button text={"이미지 업로드"} />
+          <Button text={"이미지 제거"} />
+        </ImageEditContainer>
+
+        <ProfileTextContainer>
+          {!isProfileEditing ? (
+            <>
+              <FatText text={"이름"} />
+              <FatText text={"한 줄 소개"} />
+              <EditButton>수정</EditButton>
+            </>
+          ) : (
+            <>
+              <Input placeholder={"이름"} />
+              <Input placeholder={"한 줄 소개"} />
+              <EditButton>저장</EditButton>
+            </>
+          )}
+        </ProfileTextContainer>
+      </Profile>
+    </Container>
+  );
 };
 
 const Container = styled.div`
@@ -12,5 +43,9 @@ const Container = styled.div`
   height: 100%;
   margin-top: 2rem;
 `;
+const Profile = styled.div``;
+const ImageEditContainer = styled.div``;
+const ProfileTextContainer = styled.div``;
+const EditButton = styled.span``;
 
 export default Setting;
