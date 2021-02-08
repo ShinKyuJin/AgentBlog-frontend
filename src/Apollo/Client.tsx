72d5fc379a3da1e706b7fd611bdda5f0dc600aaa
전현성 <skyoun97@gmail.com>
2021-02-09 00:29:34 +0900

    Setting - Cross Domain Cokie Sharing

--- src/Apollo/Client.tsx
+++ src/Apollo/Client.tsx
@@ -4,11 +4,6 @@ import { defaults, resolvers } from "./LocalState";
 export const serverUri = "https://agent-blog.herokuapp.com";
 //"https://agent-blog.herokuapp.com"; //"http://localhost:4000";
 
-// const link = createHttpLink({
-//   uri: '/graphql',
-//   credentials: 'same-origin'
-// });
-
 export default new ApolloClient({
   uri: serverUri,
   clientState: {
@@ -18,4 +13,5 @@ export default new ApolloClient({
   headers: {
     Authorization: `Bearer ${localStorage.getItem("token")}`,
   },
+  credentials: "include",
 });
